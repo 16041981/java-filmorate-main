d64616a0f306abc2f05ae71d2adb51bf296c6746
manager.shambala <lachins@mail.ru>
2024-06-14 01:39:52 +0200

    Переделанны классы Storage

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,17 +1,12 @@
-MERGE INTO film_mpa (mpa_id, mpa_rating)
-VALUES (1, 'G');
-MERGE INTO film_mpa (mpa_id, mpa_rating)
-VALUES (2, 'PG');
-MERGE INTO film_mpa (mpa_id, mpa_rating)
-VALUES (3, 'PG-13');
-MERGE INTO film_mpa (mpa_id, mpa_rating)
-VALUES (4, 'R');
-MERGE INTO film_mpa (mpa_id, mpa_rating)
-VALUES (5, 'NC-17');
+insert into mpas (name) values ('G');
+insert into mpas (name) values ('PG');
+insert into mpas (name) values ('PG-13');
+insert into mpas (name) values ('R');
+insert into mpas (name) values ('NC-17');
 
-MERGE INTO genre (genre_type) VALUES ('Комедия');
-MERGE INTO genre (genre_type) VALUES ('Драма');
-MERGE INTO genre (genre_type) VALUES ('Мультфильм');
-MERGE INTO genre (genre_type) VALUES ('Триллер');
-MERGE INTO genre (genre_type) VALUES ('Документальный');
-MERGE INTO genre (genre_type) VALUES ('Боевик');
+insert into genres (name) values ('Комедия');
+insert into genres (name) values ('Драма');
+insert into genres (name) values ('Мультфильм');
+insert into genres (name) values ('Триллер');
+insert into genres (name) values ('Документальный');
+insert into genres (name) values ('Боевик');
