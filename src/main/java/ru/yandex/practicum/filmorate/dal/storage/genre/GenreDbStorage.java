8d8b19d5f3ae167fe52fefd78b9c299bc1de1761
manager.shambala <lachins@mail.ru>
2024-06-14 02:00:23 +0200

    Переделанны классы Storage2

--- src/main/java/ru/yandex/practicum/filmorate/dal/storage/genre/GenreDbStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/dal/storage/genre/GenreDbStorage.java
@@ -7,7 +7,6 @@ import ru.yandex.practicum.filmorate.dal.mappers.GenreRowMapper;
 import ru.yandex.practicum.filmorate.model.Genre;
 
 import java.util.Collection;
-import java.util.List;
 
 @Component
 @Qualifier("GenreDbStorage")
