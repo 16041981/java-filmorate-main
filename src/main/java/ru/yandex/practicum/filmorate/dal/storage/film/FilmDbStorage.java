4d599f1e9989c39250d4813c2de1a796acc8558e
15151515 <kaz.sergejs@yandex.by>
2024-06-18 10:09:33 +0300

    запрос всех фильмов сломался 2

--- src/main/java/ru/yandex/practicum/filmorate/dal/storage/film/FilmDbStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/dal/storage/film/FilmDbStorage.java
@@ -1,6 +1,5 @@
 package ru.yandex.practicum.filmorate.dal.storage.film;
 
-import lombok.RequiredArgsConstructor;
 import org.springframework.jdbc.core.BatchPreparedStatementSetter;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.support.GeneratedKeyHolder;
