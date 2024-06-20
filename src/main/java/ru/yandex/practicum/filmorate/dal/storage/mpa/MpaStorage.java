7aead0b6d08220d3de2d7d6370b104871032058a
manager.shambala <lachins@mail.ru>
2024-06-20 20:54:43 +0200

    User прошел тесты 68

--- src/main/java/ru/yandex/practicum/filmorate/dal/storage/mpa/MpaStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/dal/storage/mpa/MpaStorage.java
@@ -2,7 +2,6 @@ package ru.yandex.practicum.filmorate.dal.storage.mpa;
 
 import ru.yandex.practicum.filmorate.model.Mpa;
 
-import java.util.Collection;
 import java.util.List;
 import java.util.Optional;
 
